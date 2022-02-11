e56e845892f2af93c8b03e499ba138d5bbffa98d
David Papp <david@rspsystems.com>
2022-02-11 13:19:19 +0100

    Fixed bcmsdh.c again for cypress-patch-47

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -917,7 +917,7 @@ static int brcmf_sdiod_probe(struct brcmf_sdio_dev *sdiodev)
 		goto out;
 	}
 
-	if (sdiodev->func1->device == SDIO_DEVICE_ID_CYPRESS_4373)
+	if (sdiodev->func2->device == SDIO_DEVICE_ID_CYPRESS_4373)
 		f2_blksz = SDIO_4373_FUNC2_BLOCKSIZE;
 
 	ret = sdio_set_block_size(sdiodev->func2, f2_blksz);
